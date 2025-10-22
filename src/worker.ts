3fceb998269fc85649e588431e8f610697d50055
Naitik Shah <n@daaku.org>
2025-10-22 10:46:00 +0400

    log some timers

--- src/worker.ts
+++ src/worker.ts
@@ -11,22 +11,28 @@ const run = async () => {
   })
   console.log(`Running SQLite3 version ${sqlite3.version.libVersion}`)
 
-  const db = new sqlite3.oo1.DB('file:local3?vfs=opfs', 'ct')
+  const db = new sqlite3.oo1.DB('file:local3?vfs=opfs', 'c')
 
   try {
+    console.time('create table')
     db.exec(`create table if not exists users(
       id integer primary key autoincrement,
       name text
     )`)
+    console.timeEnd('create table')
+    console.time('insert')
     db.exec({
       sql: `insert into users (name) values ($name)`,
       bind: { $name: NAMES[Math.floor(Math.random() * NAMES.length)] },
     })
+    console.timeEnd('insert')
+    console.time('select')
     const values = db.exec({
       sql: 'SELECT id, name FROM users',
       rowMode: 'object',
       returnValue: 'resultRows',
     })
+    console.timeEnd('select')
     self.postMessage(values)
   } finally {
     db.close()
